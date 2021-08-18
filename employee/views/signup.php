45ceb259f001fb01c3732fa1593ec00fff8cf797
kaderabdul22 <mostafakamal9300@gmail.com>
2021-08-18 18:45:55 +0600

    design login,signup page

--- employee/views/signup.php
+++ employee/views/signup.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Portals</title>
+	<title>Signup Portals</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,7 +14,7 @@
   body{background-color:#343a40}
   h1{color: #fff;}
   hr{border:2px solid #222}
-  .login-form{width:340px;margin:50px auto}
+  .login-form{width:340px;margin:20px auto}
   .login-form form{margin-bottom:15px;background:#f7f7f7;box-shadow:0 2px 2px rgba(0,0,0,.3);padding:30px}
   .login-form h2{margin:0 0 15px}
   .form-control, .btn{min-height:38px;border-radius:2px;font-size:15px;font-weight:bold;}
@@ -27,10 +27,16 @@
 <br>
 <div class="login-form">
     <form action="/index.php" method="post">
-        <h2 class="text-center">Log in Panel<hr></h2>       
+        <h2 class="text-center">Signup Panel<hr></h2>       
         <div class="form-group">
             <input type="text" class="form-control" placeholder="Username" name="username" required>
         </div><br>
+        <select class="form-select" aria-label="Default select example" name="department">
+            <option selected>select department</option>
+            <option value="1">Account</option>
+            <option value="2">Management</option>
+            <option value="3">Finance</option>
+        </select><br>
         <div class="form-group">
             <input type="password" class="form-control" placeholder="Password" name="password" required>
         </div><br>
@@ -47,8 +53,8 @@
             </label>
         </div><br>
         <div class="form-group">
-            <button type="submit" class="btn btn-success">Log in</button>
-            <a href="signup">Create New Account</a>
+            <button type="submit" class="btn btn-success">signin</button>
+            <a href="login">Login</a>
         </div>
     </form>
 </div>
