f5d4dcba598d50813ffd93a18ee512b5c44802c8
kaderabdul79 <kaderabdul7980@gmail.com>
2021-11-12 14:39:55 +0600

    upload post form has been design

--- employee/views/uploadPost.php
+++ employee/views/uploadPost.php
@@ -1,41 +1,49 @@
-<form method="POST" action="" enctype="multipart/form-data">
+<?php 
+include('master.php');
+include('header.php');
+?>
+<form method="POST" action="publishPostCheck.php" enctype="multipart/form-data">
      <div class="form-group">
-         <label for="username">Post Title</label>
+         <label for="posttitle">Post Title</label>
          <input type="text" class="form-control" placeholder="posttitle" name="posttitle" value="">
       </div>
-<!-- dropdown for department -->
-  <br><div class="form-group col-md-8">
-      <select id="inputState" class="form-control" name="department">
-        <!-- value=<?= $row['department']; ?> -->
-            <option <?= $row['department']; ?> selected ><?= $row['department']; ?></option>
-            <option value="account">Account</option>
-            <option value="management">Management</option>
-            <option value="finance">Finance</option>
-      </select>
+      <!-- text area start -->
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Post Description</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <!-- text area end -->
+      <!-- upload post thumbnail -->
+      <div class="form-group">
+            Upload Post Thumbnail <br><input type="file" accept="image/png, image/jpeg" name="thumbnail" id="thumbnail" value="">
     </div><br>
-<!-- dropdown for department end -->
-    Accounttype <div class="form-check">
-            <input class="form-check-input" type="radio" name="accounttype" value="employee" id="Radio1">
+     <!-- upload post thumbnail end -->
+     <!-- author name -->
+     <div class="form-group">
+         <label for="authorname">Author Name</label>
+         <input type="text" class="form-control" placeholder="authorname" name="authorname" value="">
+      </div>
+      <!-- author name end -->
+ <!-- start tags radio button -->
+  Select Tags <br>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="" value="" id="Radio1">
             <label class="form-check-label" for="Radio1">
-                <?= ($row['accounttype'] == 'employee') ? 
-                '<div class="disabled">
-              <input class="form-check-input" type="radio" name="accounttype" id="exampleRadios3" value="admin" disabled>
-              <label class="form-check-label" for="exampleRadios3">Admin</label>
-            </div>'
-                : '<input class="form-check-input" type="radio" name="accounttype" value="employee" id="Radio2">
-            <label class="form-check-label" for="Radio2">
-                Employee
-            </label>' ?>
+                viralpost
             </label><br>
-    </div>
-  <!-- end radio button -->
-  <div class="form-group">
-            Upload Picture <br><input type="file" accept="image/png, image/jpeg" name="picture" id="picture" value="<?= $row['picture']; ?>">
-    </div><br>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Confirm to Update Profile?</label>
-  </div>
-  <input type="submit" name="EditProfile" value="Edit Profile">
-  <!-- <button type="submit" class="btn btn-primary">Submit</button> -->
+            <input class="form-check-input" type="radio" name="" value="" id="Radio1">
+            <label class="form-check-label" for="Radio1">
+                bangladesh
+            </label><br>
+            <input class="form-check-input" type="radio" name="" value="" id="Radio1">
+            <label class="form-check-label" for="Radio1">
+                facebook
+            </label><br>
+            <input class="form-check-input" type="radio" name="" value="" id="Radio2">
+            <label class="form-check-label" for="Radio2">
+                admissiontest
+            </label>
+        </div><br>
+  <!-- end tags radio button -->
+  <input type="submit" name="uploadpost" value="uploadpost">
 </form>
