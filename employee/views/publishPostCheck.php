896d56f98dc6ec8103fcda843e8e762cdb8bd081
kaderabdul79 <kaderabdul7980@gmail.com>
2021-11-12 16:13:08 +0600

    user can publish post

--- employee/views/publishPostCheck.php
+++ employee/views/publishPostCheck.php
@@ -3,7 +3,7 @@ require_once('db.php');
 if(isset($_POST['uploadpost'])){
     $posttitle = $_POST['posttitle'];
     $description = $_POST['description'];
-    $thumbnail = $_FILES['thumbnail'];
+    $thumbnail = $_FILES['thumbnail']['name'];
     $authorname = $_POST['authorname'];
     
     if(isset($_POST['viralpost']) || isset($_POST['admissiontest']) || isset($_POST['facebook']) || isset($_POST['bangladesh'])){
@@ -11,6 +11,24 @@ if(isset($_POST['uploadpost'])){
         $admissiontest = isset($_POST['admissiontest']);
         $facebook = isset($_POST['facebook']);
         $bangladesh = isset($_POST['bangladesh']);
+        $postinfo = [
+            'title' => $posttitle,
+            'thumbnail' => $thumbnail,
+            'tags' => 'popularpost',
+            'author' => $authorname,
+            'description' => $description
+        ];
+        // DB connection and insert to post table
+
+        $conn = getConnection();
+        $sql = "insert into posts values('','{$postinfo['title']}','{$postinfo['thumbnail']}','{$postinfo['tags']}','{$postinfo['author']}','{$postinfo['description']}','','3-9-2021')";
+        if(mysqli_query($conn,$sql)){
+            header('Location: homepage.php');
+        }else{
+            echo "failed to insert";
+            print_r($sql);
+        
+        }
     //     $tags = [
     //     'viralpost' => $viralpost,
     //     'admissiontest' => $admissiontest,
@@ -22,27 +40,12 @@ if(isset($_POST['uploadpost'])){
     //         for($value;$value > 2;$value++){
     //             echo $key;
     //         }echo $value;echo $key;
-            
-    //     }
-    // }else{
-    //     echo "please select tag";
-    // }
-    //$key.' '.
-    // $viralpost = $_POST['viralpost'];
-    // $admissiontest = $_POST['admissiontest'];
-    // $facebook = $_POST['facebook'];
-    // $bangladesh = $_POST['bangladesh'];
-    // $tags = [
-    //     'viralpost' => '$viralpost',
-    //     'admissiontest' => '$admissiontest',
-    //     'facebook' => '$facebook',
-    //     'bangladesh' => '$bangladesh'
-    // ];
-    // foreach ($tags as $key => $value) {
-    //     echo $key.' '.$value;
-    // }
-    // echo $viralpost;
-    echo $posttitle;
+      
+    }else{
+        echo "please select tag";
+    }
+    //db tale id title thumbnail tags author description published_date updated_date
+
 }else{
     header('Location: uploadPost.php');
 }
